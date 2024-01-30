9767f9ba50c7ab3362df672cf85ebdd0f0fe588a
aa <t3841@yandex.ru>
2024-01-30 17:44:49 +0400

    add recipe crud

--- backend_api/backend_api/urls.py
+++ backend_api/backend_api/urls.py
@@ -8,6 +8,9 @@ from backend_api.swagger import schema_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
+    path('api/recipes', include('recipes.urls')),
+
+    # TOKENS
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
