6a6475e6798da1323040b29c575ac387c3e0390c
aa <t3841@yandex.ru>
2024-02-02 00:22:42 +0400

    Добавил отдачу статики

--- backend_api/recipes/models.py
+++ backend_api/recipes/models.py
@@ -6,9 +6,6 @@ from rest_framework.exceptions import ValidationError
 from users.models import User
 
 
-
-
-
 def generate_filename(instance, filename):
     """Функция для генерации имени файла на основе UUID."""
     extension = filename.split('.')[-1]  # получаем расширение файла
@@ -37,6 +34,7 @@ class Recipe(models.Model):
     # description = models.TextField(max_length=1500)
     # category = models.ForeignKey(Category, on_delete=models.PROTECT) # категорию невозможно удалить пока есть связанные с ней рецепты
     # category = models.ManyToManyField(Category)
+
     image = models.ImageField(upload_to=generate_filename,
                               validators=[
                                   validators.FileExtensionValidator(['png', 'jpg', 'jpeg']),
@@ -46,8 +44,40 @@ class Recipe(models.Model):
     # ingredients = models.TextField(max_length=1500)
     # cooking_time_in_minutes = models.IntegerField(
     #     default=0,
-    #     validators=[MinValueValidator(1), MaxValueValidator(1440)])
+    #     validators=[validators.MinValueValidator(1), validators.MaxValueValidator(1440)])
     # published_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+"""Для добавления категории по имени, вам необходимо выполнить следующие шаги:
+
+1. Получите или создайте объект категории по имени, используя метод `get_or_create` модели `Category`:
+
+```python
+category, created = Category.objects.get_or_create(name='Название категории')
+```
+
+2. Добавьте полученный объект категории в поле `category` экземпляра модели, используя метод `add`:
+
+```python
+<your_model_instance>.category.add(category)
+```
+
+Пример использования:
+
+```python
+# Получить или создать объект категории по имени
+category, created = Category.objects.get_or_create(name='Название категории')
+
+# Создать объект модели
+my_object = MyModel.objects.create(...)
+
+# Добавить категорию к объекту модели
+my_object.category.add(category)
+```
+
+При использовании метода `get_or_create`, будут созданы новые объекты категории, 
+если объект с указанным именем не существует. 
+Если объект уже существует, метод вернет существующий объект."""
--- backend_api/recipes/serializers.py
+++ backend_api/recipes/serializers.py
@@ -1,20 +1,30 @@
 from rest_framework import serializers
-from .models import Recipe, Category
-
-
-class CategoryListSerializer(serializers.ListSerializer):
-    def to_representation(self, data):
-        return [item.name for item in data]
+from .models import Category, Recipe
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['name']
-        list_serializer_class = CategoryListSerializer
 
 class RecipeSerializer(serializers.ModelSerializer):
-    # category = CategorySerializer()
+    # category = CategorySerializer(many=True)
     class Meta:
         model = Recipe
         fields = '__all__'
-        #read_only_fields = ('user',) #включить при активном запросе токена на эндпойнт recipe
+        # read_only_fields = ('user',)
+
+    # def create(self, validated_data):
+    #     categories_data = validated_data.pop('category')
+    #     recipe = Recipe.objects.create(**validated_data)
+    #
+    #     for category_data in categories_data:
+    #         category_name = category_data.get('name')
+    #         category, created = Category.objects.get_or_create(name=category_name)
+    #         recipe.category.add(category)
+    #
+    #     return recipe
+    #
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     representation['category'] = [category.name for category in instance.category.all()]
+    #     return representation
